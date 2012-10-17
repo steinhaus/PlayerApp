8e378e1f587b799b1f4e7fa36946131cb221229a
Felipe Gringo <FGringo@Felipe-Gringos-MacBook-Pro.local>
2012-10-17 13:34:20 -0300

    Initial Commit

---
+++ PlayerProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlayerProject
+//
+//  Created by Felipe Gringo on 10/17/12.
+//  Copyright (c) 2012 Felipe Gringo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlayerProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlayerProject
+//
+//  Created by Felipe Gringo on 10/17/12.
+//  Copyright (c) 2012 Felipe Gringo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
